48215cfd5db9488d8ec570506690d51d921e7f01
Dave Beaumont <david.beaumont@network.elancoah.com>
2021-10-28 10:03:52 +0100

    Fixed eslint errors.

--- web-boilerplate-ui/src/App.tsx
+++ web-boilerplate-ui/src/App.tsx
@@ -1,27 +1,22 @@
-import React from 'react';
 import logo from './assets/logo.svg';
 import './styles/app.css';
 
-function App() {
-  console.log('test')
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () =>
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h3>React Web Boilerplate</h3>
+      {process.env.REACT_APP_ENVIRONMENT_VARIABLE &&
+        <>
+          <p>
+            Environment Variables Loaded Sucessfully.
+          </p>
+          <p>
+            {process.env.REACT_APP_ENVIRONMENT_VARIABLE}
+          </p>
+        </>
+      }
+    </header>
+  </div>
 
 export default App;
