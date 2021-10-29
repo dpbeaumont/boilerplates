e429fef67ed008be714bf13836f3b05f951b515f
Dave Beaumont <david.beaumont@network.elancoah.com>
2021-10-29 16:28:32 +0100

    airbnb eslint approach

--- web-boilerplate-ui/.eslintrc.js
+++ web-boilerplate-ui/.eslintrc.js
@@ -1,25 +1,25 @@
 module.exports = {
 	extends: [
+		"airbnb",
 		'prettier'
 	],
-	plugins: [
-	],
+	"plugins": ["react"],
 	parserOptions: {
 		project: ['./tsconfig.json'],
 		tsconfigRootDir: __dirname,
 	},
 	env: {
-	} ,
+	},
 	ignorePatterns: ["setupTests.js"],
 	settings: {
-    "import/resolver": {
-      typescript: {}
-    },
-  },
-  rules: {
-	  'max-classes-per-file': 0,
-	  'no-restricted-syntax': 0,
-	  'no-await-in-loop': 0,
+		"import/resolver": {
+			typescript: {}
+		},
+	},
+	rules: {
+		'max-classes-per-file': 0,
+		'no-restricted-syntax': 0,
+		'no-await-in-loop': 0,
 
-  }
+	}
 }
